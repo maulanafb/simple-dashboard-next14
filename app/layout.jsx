20ff51d1d71e46dba6bac88f98d4bea44959bad8
unknown <gagas112212@gmail.com>
2023-12-06 17:19:21 +0700

    useless button animation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './ui/globals.css'
-// import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from 'nextjs-toploader';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <NextTopLoader /> */}
+        <NextTopLoader />
         {children}
       </body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link"
-
+import styles from "./home.module.css"
 const Homepage = () => {
   return (
     <div style={{ display: 'flex', justifyContent: "center", alignItems: "center", flexDirection: "row", height: "100vh", borderRadius: "100px" }}>
-      <div style={{ padding: "5px 10px", backgroundColor: "teal", borderRadius: "50px" }}>
+      <div style={{ padding: "5px 10px", backgroundColor: "teal", borderRadius: "50px" }} className={styles.button}>
         <Link href={'/dashboard'}>
           <h1>Goto dashboard</h1>
         </Link>
